43aa5fc09706a5394db4373dbcdee73b38c47bef
Roli <kocsisr1@kkszki.hu>
2025-09-18 10:26:37 +0200

    Tömb elemeinek bekérése és kiírása for illetve foreach ciklussal

--- ArryExample/Program.cs
+++ ArryExample/Program.cs
@@ -10,6 +10,24 @@ namespace ArryExample
     {
         static void Main(string[] args)
         {
+            int[] numbers = new int[5];
+            //Az elemek bekérése egyesével
+            //numbers[0] = int.Parse(Console.ReadLine());
+            //numbers[1] = 2;
+            //numbers[2] = 3;
+            //numbers[3] = 4;
+            //numbers[4] = 5;
+
+            //Az adatok bekérése ciklussal
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"Kérem a(z) {i}. elemet: ");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            foreach (int item in numbers)
+            {
+                Console.Write(item + " ");
+            }
         }
     }
 }
